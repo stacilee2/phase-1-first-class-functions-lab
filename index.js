29756e69c10b70dc3c55e05eaf0a35b1bec467a0
Staci Rosales <stacileep2@gmail.com>
2022-10-03 09:35:09 -0500

    commit tree d8819b373dac78732d92267ee0aab5ed9953824f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+console.log(drivers);
+
+const returnFirstTwoDrivers = (array) => array.slice(0, 2)
+
+const returnLastTwoDrivers = (array) => array.slice(-2)
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+console.log(selectingDrivers);
+
+function createFareMultiplier(integer) {
+    return (fare) => integer * fare
+}
+//fareDoubler() — Declare a variable with const and assign a function returned by createFareMultiplier() to it. 
+//Invoke createFareMultiplier() in such a way that the new 
+//fareDoubler() function accepts a fare as its lone argument and doubles it.
+
+const fareDoubler = createFareMultiplier(2) 
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers)
+}
